2ded897a0df3a118b7d3d9daafef00c177925acf
Ole Magnus Fon Johnsen <me@omfj.no>
2025-06-15 01:12:12 +0200

    Add styles

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use dotman::{Dotman, DotmanConfig};
 use std::path::PathBuf;
 
 #[derive(Parser, Debug)]
+#[command(styles = get_styles())]
 struct Cli {
     #[clap(subcommand)]
     command: Command,
@@ -93,4 +94,44 @@ fn main() {
             }
         }
     }
+}
+
+// Thank you to:
+// https://stackoverflow.com/a/76916424/8653870
+pub fn get_styles() -> clap::builder::Styles {
+    clap::builder::Styles::styled()
+        .usage(
+            anstyle::Style::new()
+                .bold()
+                .underline()
+                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Yellow))),
+        )
+        .header(
+            anstyle::Style::new()
+                .bold()
+                .underline()
+                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Yellow))),
+        )
+        .literal(
+            anstyle::Style::new().fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Green))),
+        )
+        .invalid(
+            anstyle::Style::new()
+                .bold()
+                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Red))),
+        )
+        .error(
+            anstyle::Style::new()
+                .bold()
+                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Red))),
+        )
+        .valid(
+            anstyle::Style::new()
+                .bold()
+                .underline()
+                .fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::Green))),
+        )
+        .placeholder(
+            anstyle::Style::new().fg_color(Some(anstyle::Color::Ansi(anstyle::AnsiColor::White))),
+        )
 }
